8c155121642ce8d4bf8336d36b20fdcf623a5203
Ruslan Nanai <ruslannanay1@gmail.com>
2022-06-07 20:23:08 +0200

    review fixes

--- basics.php
+++ basics.php
@@ -1,21 +1,23 @@
 <?php
 
-$roomLength = 5; // длина
-$roomWidth = 3; // ширина
-$roomHeight = 4; // высота
-$ceilingFinishCost = 175; // при цене 175 за м2
-$flooringCost = 350; // при цене 350 за м2
-$wallPlasteringCost = 200; // при цене 200 за м2
+$roomLength = 5;
+$roomWidth = 3;
+$roomHeight = 4;
+$ceilingFinishCost = 175;
+$flooringCost = 350;
+$wallPlasteringCost = 200;
 
 $floorAndCeilingArea = 5 * 3;
-$WallArea = 4 * 5;
+$wallArea = 4 * 5;
 $ceilingFinishCost = 175 * 15;
 $flooringCost = 350 * 15;
 $wallPlasteringCost = 200 * 20;
 
 
 echo "Floor and ceiling area: {$floorAndCeilingArea}. <br />";
-echo "Wall area: {$WallArea}. <br />";
+echo "Wall area: {$wallArea}. <br />";
 echo "Ceiling finish cost: {$ceilingFinishCost}. <br />";
 echo "Flooring cost: {$flooringCost}. <br />";
 echo "Wall plastering cost: {$wallPlasteringCost} <br />";
+
+echo 'I studying ' . 'stings ' . 'concatenation.';
