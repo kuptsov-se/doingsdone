eb83bb553a669c4ed048ee6ec1926307ad06310a
Ruslan Nanai <ruslannanay1@gmail.com>
2022-06-13 15:02:13 +0200

    first stage

---
+++ arrays.php
@@ -0,0 +1,12 @@
+<?php
+
+
+
+$students = [
+    'id001' => ['Ruslan Nanai', 'Maths 3', 'Ukrainian 4', 'English 3'],
+    'id002' => ['Sergei Ivanoff', 'Maths 4', 'Ukrainian 5', 'English 4'],
+    'id003' => ['Andrey Markeloff', 'Maths 3', 'Ukrainian 4', 'English 5'],
+    'id004' => ['Anton Prokopenko', 'Maths 5', 'Ukrainian 5', 'English 5'],
+    'id005' => ['Valentin Sergienko', 'Maths 3', 'Ukrainian 4', 'English 3']
+];
+
