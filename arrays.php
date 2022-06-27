5e725bb3f8111ca8b571a67c752a48c7b7aaed92
Ruslan Nanai <ruslannanay1@gmail.com>
2022-06-28 01:49:39 +0200

    calculated the average grade of students

--- arrays.php
+++ arrays.php
@@ -37,23 +37,15 @@ $students = [
         'eng' => 4,
     ],
 ];
-    foreach ($students as $studentInfo) {
-        echo 'Student ' . $studentInfo['name'] . '<br>';
-        foreach ($studentInfo as $fieldName => $studentData) {
-            echo $fieldName . ': ' . $studentData . '<br>';
-        }
-
+foreach ($students as $studentInfo) {
+    echo 'Student id: ' . $studentInfo['id'] . '<br>';
+    echo 'Student: ' . $studentInfo['name'] . '<br>';
+    //foreach ($students as $studentInfo => $studentData) {
+        $mathMark = $studentInfo ['math'];
+        $ukrMark = $studentInfo ['ukr'];
+        $engMark = $studentInfo ['eng'];
+        $marksSum = $mathMark + $ukrMark + $engMark;
+        $avg = $marksSum / 3;
+        echo 'AVG mark: ' . $avg . '<br>';
 
 }
-$avg1 = (3 + 4 + 3) / 3;
-    echo "AVGid 1 : $avg1 <br>";
-    $avg2 = (5 + 5 + 4) / 3;
-    echo "AVGid 2 : $avg2 <br>";
-    $avg3 = (3 + 4 + 3) / 3;
-    echo "AVGid 3 : $avg3 <br>";
-    $avg4 = (4 + 4 + 4) / 3;
-    echo "AVGid 4 : $avg4 <br>";
-    $avg5 = (5 + 4 + 4) / 3;
-    echo "AVGid 5 : $avg5 <br>";
-
-
