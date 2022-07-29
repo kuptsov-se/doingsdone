6887253bc599d07c1af04af281890b0025c7b533
Ruslan Nanai <ruslannanay1@gmail.com>
2022-07-29 16:03:20 +0200

    created arrays of faculties, groups and students

---
+++ university.php
@@ -0,0 +1,154 @@
+<?php
+$faculty = [
+    [
+        'id' => 1,
+        'title' => 'Biological',
+    ],
+    [
+        'id' => 2,
+        'title' => 'Chemical',
+    ],
+    [
+        'id' => 3,
+        'title' => 'Geographical',
+    ],
+    [
+        'id' => 4,
+        'title' => 'Geological',
+    ],
+    [
+        'id' => 5,
+        'title' => 'Medical',
+    ],
+];
+$groupsOfStudents = [
+    [
+        'id' => 1,
+        'title' => 'Bio - 19',
+        'faculty' => 'Biological',
+    ],
+    [
+        'id' => 2,
+        'title' => 'Bio - 20',
+        'faculty' => 'Biological',
+    ],
+    [
+        'id' => 3,
+        'title' => 'Bio - 21',
+        'faculty' => 'Biological',
+    ],
+    [
+        'id' => 4,
+        'title' => 'Chm - 19',
+        'faculty' => 'Chemical',
+    ],
+    [
+        'id' => 5,
+        'title' => 'Chm - 20',
+        'faculty' => 'Chemical',
+    ],
+    [
+        'id' => 6,
+        'title' => 'Chm - 21',
+        'faculty' => 'Chemical',
+    ],
+    [
+        'id' => 7,
+        'title' => 'Geo - 19',
+        'faculty' => 'Geographical',
+    ],
+    [
+        'id' => 8,
+        'title' => 'Geo - 20',
+        'faculty' => 'Geographical',
+    ],
+    [
+        'id' => 9,
+        'title' => 'Geo - 21',
+        'faculty' => 'Geographical',
+    ],
+    [
+        'id' => 10,
+        'title' => 'Geolog - 19',
+        'faculty' => 'Geological',
+    ],
+    [
+        'id' => 11,
+        'title' => 'Geolog - 20',
+        'faculty' => 'Geological',
+    ],
+    [
+        'id' => 12,
+        'title' => 'Geolog - 21',
+        'faculty' => 'Geological',
+    ],
+    [
+        'id' => 13,
+        'title' => 'Med - 19',
+        'faculty' => 'Medical',
+    ],
+    [
+        'id' => 14,
+        'title' => 'Med - 20',
+        'faculty' => 'Medical',
+    ],
+    [
+        'id' => 15,
+        'title' => 'Med - 21',
+        'faculty' => 'Medical',
+    ],
+];
+$items = [
+    [
+        'id' => 1,
+        'title' => 'Ukrainian language',
+    ],
+    [
+        'id' => 2,
+        'title' => 'Maths',
+    ],
+    [
+        'id' => 3,
+        'title' => 'Informatics',
+    ],
+    [
+        'id' => 4,
+        'title' => 'Physics',
+    ],
+    [
+        'id' => 5,
+        'title' => 'History',
+    ],
+    [
+        'id' => 6,
+        'title' => 'Philosophy',
+    ],
+    [
+        'id' => 7,
+        'title' => 'Psychology',
+    ],
+    [
+        'id' => 8,
+        'title' => 'Fundamentals of life safety',
+    ],
+    [
+        'id' => 9,
+        'title' => 'Economic theory',
+    ],
+    [
+        'id' => 10,
+        'title' => 'English language',
+    ],
+    [
+        'id' => 11,
+        'title' => 'Physical culture',
+    ],
+];
+$students = [
+    [
+        'id' => 1,
+        'name' => 'Natalya',
+        'surname' => 'Aleshina',
+
+    ],
+];
